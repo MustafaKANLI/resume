86c3f27f181dde763b6a54dea9d0a7abd88ff0f3
MusatafaKANLI <mustafakanli98@gmail.com>
2022-06-20 23:47:07 +0300

    added pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from "./logo.svg";
+import React from "react";
+import { Routes, Route, Router } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Skills from "./Pages/Skills/Skills";
+import Projects from "./Pages/Projects/Projects";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
