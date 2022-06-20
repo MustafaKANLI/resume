5276f370bc26c0152b81b516fa7bb1573eba27b4
MusatafaKANLI <mustafakanli98@gmail.com>
2022-06-20 22:55:49 +0300

    added navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import classes from "./Navbar.module.css";
+
+const Navbar = (props) => {
+  return (
+    <header className={classes.header}>
+      <div className={classes.navBar}>
+        <Link to="/">
+          <img className={classes.logo} src={Logo} />
+        </Link>
+
+        <Link to="/skills">
+          <div className={classes.link}>Skills</div>
+        </Link>
+
+        <Link to="/projects">
+          <div className={classes.link}>Projects</div>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
