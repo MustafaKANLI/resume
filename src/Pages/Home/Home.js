780ca6827fe6aec8f645b11040a7a8453ed5233b
MusatafaKANLI <mustafakanli98@gmail.com>
2022-06-25 14:27:49 +0300

    video size fixed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,19 +1,12 @@
 import React from "react";
 import classes from "./Home.module.css";
 import About from "../../components/About/About";
-import video from "../../resources/Gifs/bg.mp4";
+import Video from "../../components/Video/Video";
 
 const Home = () => {
   return (
     <div>
-      <div className={classes.videoDiv}>
-        <h1 className={classes.nameTitle}>Mustafa Nur KANLI</h1>
-        <div className={classes.grey}></div>
-        <video className={classes.video} autoPlay muted loop id="myVideo">
-          <source src={video} type="video/mp4" />
-        </video>
-      </div>
-
+      <Video></Video>
       <About></About>
     </div>
   );
---
+++ src/components/Video/Video.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from "./Video.module.css";
+import video from "../../resources/Gifs/bg.mp4";
+
+const Video = () => {
+  return (
+    <div className={classes.videoDiv}>
+      <h1 className={classes.nameTitle}>Mustafa Nur KANLI</h1>
+      <div className={classes.explainDiv}>
+        <h3>I'm </h3>
+        <h3 className={classes.explainTitle}>Computer Science Engineer</h3>
+      </div>
+      <div className={classes.grey}></div>
+      <div className={classes.videoBg}>
+        <video className={classes.video} autoPlay muted loop id="myVideo">
+          <source src={video} type="video/mp4" />
+        </video>
+      </div>
+    </div>
+  );
+};
+
+export default Video;
