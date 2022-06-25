13c4b0106732df66781eac0a87b230402ab39a84
MusatafaKANLI <mustafakanli98@gmail.com>
2022-06-25 15:56:03 +0300

    about moved bottom of the picture

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,17 +5,67 @@ import classes from "./About.module.css";
 const About = () => {
   return (
     <div className={classes.wrapper}>
-      <div>
-        <img className={classes.profile} src={Profile} />
-      </div>
-      <div>
-        <h3>Computer Science Engineer</h3>
-        <div>
-          I am a third year student at Akdeniz University Computer Engineering.
-          I am a person who constantly researches, develops himself, loves
-          listening to music and doing sports, has entrepreneurial ideas,
-          invests in his own future, and can easily adapt.
+      <div className={classes.info}>
+        <div className={classes.profilePicDiv}>
+          <img className={classes.picture} src={Profile} />
         </div>
+
+        <div className={classes.about}>
+          <h3>Computer Science Engineer</h3>
+          <div className={classes.listDiv}>
+            <div>
+              <ul>
+                <li>
+                  <p>
+                    <strong>Birthday: </strong>
+                    08 November 1999
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    <strong>Phone: </strong>
+                    +90 553 588 5367
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    <strong>City: </strong>
+                    Antalya / Turkey
+                  </p>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>
+                  <p>
+                    <strong>Birthday: </strong>
+                    08 November 1999
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    <strong>Phone: </strong>
+                    +90 553 588 5367
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    <strong>City: </strong>
+                    Antalya / Turkey
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        I am a third year student at Akdeniz University Computer Engineering. I
+        am a person who constantly researches, develops himself, loves listening
+        to music and doing sports, has entrepreneurial ideas, invests in his own
+        future, and can easily adapt.
       </div>
     </div>
   );
--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -4,14 +4,14 @@ import video from "../../resources/Gifs/bg.mp4";
 
 const Video = () => {
   return (
-    <div className={classes.videoDiv}>
+    <div className={classes.videoWrapper}>
       <h1 className={classes.nameTitle}>Mustafa Nur KANLI</h1>
       <div className={classes.explainDiv}>
         <h3>I'm </h3>
         <h3 className={classes.explainTitle}>Computer Science Engineer</h3>
       </div>
       <div className={classes.grey}></div>
-      <div className={classes.videoBg}>
+      <div className={classes.videoDiv}>
         <video className={classes.video} autoPlay muted loop id="myVideo">
           <source src={video} type="video/mp4" />
         </video>
