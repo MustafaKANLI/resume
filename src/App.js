76f25ed05bd57b282680ad2553feeb82b11cc993
MusatafaKANLI <mustafakanli98@gmail.com>
2022-08-03 22:15:35 +0300

    Skills removed from app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, useRoutes, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
-import Skills from "./Pages/Skills/Skills";
 import Projects from "./Pages/Projects/Projects";
 
 import classes from "./App.module.css";
@@ -10,7 +9,6 @@ const App = () => {
   let routes = useRoutes([
     { path: "*", element: <Navigate to="/" /> },
     { path: "/", element: <Home /> },
-    { path: "/skills", element: <Skills /> },
     { path: "/projects", element: <Projects /> },
   ]);
   return routes;
